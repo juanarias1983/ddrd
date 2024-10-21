c337c80c9656fa243d63ec9086f2e563d05e382a
juanarias1983 <72908317+juanarias1983@users.noreply.github.com>
2024-10-21 07:20:04 +0200

    animación porcentaje

--- dashboard-docudocu/assets/js/docudocu2024.js
+++ dashboard-docudocu/assets/js/docudocu2024.js
@@ -4,3 +4,18 @@ document.addEventListener("DOMContentLoaded", function() {
         hotBadge.textContent = "Comprar"; // Cambia el texto aquí
     }
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const h1 = document.querySelector('.textoFill');
+    const percentage = h1.getAttribute('data-text');
+    const fillValue = `${percentage}%`;
+
+    // Establece el porcentaje como variable CSS
+    h1.style.setProperty('--fill-percentage', fillValue);
+
+    // Activa la animación después de un pequeño retraso
+    setTimeout(() => {
+        h1.classList.add('animate');
+    }, 100);
+});
